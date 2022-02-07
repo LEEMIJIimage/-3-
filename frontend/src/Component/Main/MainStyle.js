2fb8fb1e8a87523ca1b34187067ef3fbcf3533a7
DESKTOP-RKMQ2M7\user <wjdtjr9401@naver.com>
2022-02-07 11:00:16 +0900

    [Add] 베경 이미지 작업 진행 중 #5

---
+++ frontend/src/Component/Main/MainStyle.js
@@ -0,0 +1,10 @@
+/*import styled from "styled-Component";
+
+const Backgroud = styled.div`
+  backgroundimage: url(./imges/imge.png);
+  width: 100%;
+  height: 100%;
+`;
+
+export default Backgroud;
+*/
