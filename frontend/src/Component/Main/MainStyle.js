a79b4374be5b1713413a7a9f55f1c65bfc2fc730
JeongYunMi <tamd5971@gmail.com>
2022-02-07 13:06:25 +0900

    Add: 백그라운드 이미지 설정 #5

--- frontend/src/Component/Main/MainStyle.js
+++ frontend/src/Component/Main/MainStyle.js
@@ -1,9 +1,13 @@
 import styled from "styled-components";
+import bgImage from "../../imges/imge.png";
 
 const Backgrond = styled.div`
-  background-image: url(./imge.png);
-  width: 100%;
-  height: 100%;
+  background-image: url(${bgImage});
+  background-repeat : no-repeat;
+  background-size : cover;
+  background-position: center center;
+  width: 100vw;
+  height: 100vh;
 `;
 
 export default Backgrond;
