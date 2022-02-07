15b27ce6be67fd828feb461e58764eca5fd930f6
JeongYunMi <tamd5971@gmail.com>
2022-02-08 05:57:20 +0900

    Add: 날짜 인풋 박스, 나가기 버튼, 애니메이션 등 진행 #7

--- frontend/src/Component/Chart/Chart.js
+++ frontend/src/Component/Chart/Chart.js
@@ -1,13 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import DatePicker from "react-datepicker";
+import "./react-datepicker.css";
 import { HiOutlineX } from "react-icons/hi";
-import { ChartWrap, GetOutModal, TitleWrap, SVG, Rect } from "./ChartStyle";
+import {
+  ChartWrap,
+  GetOutModal,
+  TitleWrap,
+  SVG,
+  Rect,
+  TitleText,
+  Middle,
+  DateButton,
+} from "./ChartStyle";
 import sendApi from "../../apis/sendApi";
 
 function Chart({ onclickGetOut }) {
+  const [receiveStartDate, setReceiveStartDate] = useState(new Date());
+  const [receiveEndDate, setReceiveEndDate] = useState(new Date());
+  // const [receiveChart, setReceiveChart] = useState({});
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
   useEffect(async () => {
-    const data = await sendApi.startEndDate();
+    const { data } = await sendApi.startEndDate();
     console.log("data", data);
+    setReceiveStartDate(new Date(data.firstData_createdAt));
+    setReceiveEndDate(new Date(data.lastData_createdAt));
+
+    console.log(receiveStartDate, receiveEndDate);
   }, []);
+
+  const onClickDateSendBtn = async () => {
+    // const { data } = await sendApi.startEndDate({"startDate": startDate, "endDate": endDate});
+    console.log("startDate", startDate, "endDate", endDate);
+  };
+
   return (
     <ChartWrap>
       <SVG
@@ -27,10 +54,27 @@ function Chart({ onclickGetOut }) {
         />
       </SVG>
       <TitleWrap>
+        <TitleText>Chart</TitleText>
         <GetOutModal onClick={onclickGetOut}>
-          <HiOutlineX color="white" size={40} />
+          <HiOutlineX color="white" size={32} />
         </GetOutModal>
       </TitleWrap>
+      <Middle>
+        <DatePicker
+          selected={receiveStartDate} // 날짜 state
+          onChange={(date) => setStartDate(date)} // 날짜 설정 콜백 함수
+          minDate={receiveStartDate}
+          maxDate={receiveEndDate}
+        />
+        ~
+        <DatePicker
+          selected={receiveEndDate} // 날짜 state
+          onChange={(date) => setEndDate(date)} // 날짜 설정 콜백 함수
+          minDate={receiveStartDate}
+          maxDate={receiveEndDate}
+        />
+        <DateButton onClick={onClickDateSendBtn}>검색</DateButton>
+      </Middle>
     </ChartWrap>
   );
 }
--- frontend/src/Component/Chart/ChartStyle.js
+++ frontend/src/Component/Chart/ChartStyle.js
@@ -9,6 +9,47 @@ const modalFadeIn = keyframes`
   }
 `;
 
+const modalContentFadeIn = keyframes`
+  0% {
+    opacity: 0;
+    color: transparent;
+  }
+  100% {
+    opacity: 1;
+    color: black;
+  }
+`;
+
+const modalTitleFadeIn = keyframes`
+  0% {
+    opacity: 0;
+    color: transparent;
+    
+  }
+  100% {
+    opacity: 1;
+    color: black;
+    border-bottom: 2px solid black;
+  }
+`;
+
+// const modalMiddleFadeIn = keyframes
+//   100% {
+//     display: flex;
+//   justify-content: center;
+//   }
+// `;
+
+// const modalTitleFadeIn = keyframes
+//   0% {
+//     opacity: 0;
+//   }
+//   100% {
+//     opacity: 1;
+//     background-color: #acbfe3;
+//   }
+// `;
+
 const sketchIn = keyframes`
   0% {
     opacity: 30%;
@@ -87,16 +128,32 @@ const ChartWrap = styled.div`
 
 const TitleWrap = styled.div`
   display: flex;
-  justify-content: flex-end;
+  justify-content: space-between;
+  padding-bottom: 16px;
+  animation: ${modalContentFadeIn} 0.5s 1.4s linear forwards;
 `;
+
+const TitleText = styled.div`
+  width: 100%;
+  margin-top: 8px;
+  display: flex;
+  justify-content: space-around;
+  font-weight: bold;
+  font-size: 32px;
+  opacity: 0;
+  margin: 0 5% 0 5%;
+  padding: 8px 0 8px 0;
+  animation: ${modalTitleFadeIn} 0.5s 1.4s linear forwards;
+`;
+
 const GetOutModal = styled.div`
   position: absolute;
   top: -24px;
   right: -24px;
-  width: 56px;
-  height: 56px;
+  width: 40px;
+  height: 40px;
   border-radius: 50%;
-  padding: 16px 0 0 16px;
+  padding: 8px 0 0 8px;
   background-color: black;
 `;
 
@@ -107,6 +164,7 @@ const SVG = styled.svg`
   height: 100%;
   width: 100%;
   border-radius: 3px;
+  z-index: -1;
 `;
 
 const Rect = styled.rect`
@@ -116,4 +174,34 @@ const Rect = styled.rect`
   animation: ${sketchIn} 0.9s 0.3s cubic-bezier(0.165, 0.84, 0.44, 1) forwards;
 `;
 
-export { ChartWrap, GetOutModal, TitleWrap, SVG, Rect };
+const Middle = styled.div`
+  opacity: 0;
+  display: flex;
+  justify-content: center;
+  animation: ${modalContentFadeIn} 0.6s 1.5s linear forwards;
+`;
+
+const DateButton = styled.button`
+  height: 38px;
+  margin: 0 16px 0 16px;
+  transition-duration: 0.4s;
+  background-color: #fff;
+  border: 1px solid black;
+  border-radius: 4px;
+  text-align: center;
+
+  &:hover {
+    background-color: black;
+    color: white;
+  }
+`;
+export {
+  ChartWrap,
+  GetOutModal,
+  TitleWrap,
+  SVG,
+  Rect,
+  TitleText,
+  Middle,
+  DateButton,
+};
