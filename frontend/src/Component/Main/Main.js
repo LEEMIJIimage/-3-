25edc97d826a2373224bf952af90982dceb24d5b
DESKTOP-RKMQ2M7\user <wjdtjr9401@naver.com>
2022-02-07 16:49:25 +0900

    Add:main UI 진행 중 #5

--- frontend/src/Component/Main/Main.js
+++ frontend/src/Component/Main/Main.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { BsFillCloudSunFill } from "react-icons/bs";
-import { Button, Top } from "./MainStyle";
+import { Button, Top, ChartBox, Bottom, Middle } from "./MainStyle";
 
 function Main() {
   return (
-    <Top>
-      <div>
-        <BsFillCloudSunFill size={150} color="white" />
-      </div>
-      <Button> Chart</Button>
-    </Top>
+    <>
+      <Top>
+        <div>
+          <BsFillCloudSunFill size={150} color="white" />
+        </div>
+        <Button> Chart</Button>
+      </Top>
+      <Middle />
+      <Bottom>
+        <ChartBox />
+        <ChartBox />
+        <ChartBox />
+        <ChartBox />
+      </Bottom>
+    </>
   );
 }
 
--- frontend/src/Component/Main/MainStyle.js
+++ frontend/src/Component/Main/MainStyle.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 const Top = styled.div`
   display: flex;
-
+  justify-content: space-between;
+  margin-left: 30px;
   margin-right: 50px;
   margin-top: 30px;
 `;
@@ -20,5 +21,16 @@ const Button = styled.button`
   text-decoration: none;
   text-shadow: 0px 1px 0px #4d3534;
 `;
-
-export { Button, Top };
+const Middle = styled.div``;
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+const ChartBox = styled.div`
+  width: 280px;
+  height: 230px;
+  background-color: white;
+  opacity: 0.4;
+  border-radius: 20px;
+`;
+export { Button, Top, ChartBox, Bottom, Middle };
