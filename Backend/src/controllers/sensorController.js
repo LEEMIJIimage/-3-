5fdd2159ebd24a2e8215592a82c6519f799608d7
haemiii <73984822+haemiii@users.noreply.github.com>
2022-02-09 12:11:50 +0900

    [ADD] chartData : post request 처리  #15

--- Backend/src/controllers/sensorController.js
+++ Backend/src/controllers/sensorController.js
@@ -31,13 +31,9 @@ export const data = async (req, res) => {
 export const startend = async (req, res) => {
   const firstData = await Sensor.findOne();
   const lastData = await Sensor.find().sort({ _id: -1 }).limit(1);
-  console.log("ddd", lastData);
 
   const firstData_createdAt = firstData.createdAt;
   const lastData_createdAt = lastData[0].createdAt;
-
-  console.log(firstData_createdAt);
-  console.log(lastData_createdAt);
 
   const startendObject = {
     firstData_createdAt,
